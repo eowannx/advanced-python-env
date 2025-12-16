37a1b073cb714f34a64cde4c225f1bb877c4335a
Adam <erdenovadam@gmail.com>
2025-12-16 13:33:37 +0500

    Add solution for task-1

---
+++ week-1/task-1.py
@@ -0,0 +1,7 @@
+#Assignment 1 - task 1
+lname, fname = input("Your last name, first name?").split()
+age = int(input("How old are you? "))
+numbers = int(input("Your phone number? "))
+print("Your first name", fname + ", last name", lname)
+print("Your age ", age)
+print("Your phone number", numbers)
